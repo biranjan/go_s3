838f02e67824374af0464bbd34febc423cfe2ef1
Biranjan <birajan.raut@gmail.com>
2020-05-03 18:24:18 +0300

    adds flag

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/biranjan/go_s3/s_3"
 )
 
 func main() {
-	nameVar := "test"
+	//nameVar := "test"
+	uploadFile := flag.String("upload", "", "file path to upload")
+	downloadFile := flag.String("download", "", "file name to download")
 
-	fmt.Println("hello", s_3.Hello(nameVar))
+	switch os.Args[1] {
+	case "upload":
+		flag.Parse()
+		fmt.Println("hello", s_3.Hello(*uploadFile))
+	case "download":
+		flag.Parse()
+		fmt.Println("hello", s_3.HandleUpload(*downloadFile))
+	default:
+		fmt.Println("Expected 'download' or 'upload' command")
+		os.Exit(1)
+
+	}
 }
