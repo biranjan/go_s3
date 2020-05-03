d28fda716c32b11c412cee847d50af7d4a0b500f
Biranjan <birajan.raut@gmail.com>
2020-05-03 18:24:18 +0300

    adds flag

--- s_3/s_3.go
+++ s_3/s_3.go
@@ -1,5 +1,11 @@
 package s_3
 
+// Function to print
 func Hello(filename string) string {
+	return filename
+}
+
+// Function to test flag
+func HandleUpload(filename string) string {
 	return filename
 }
